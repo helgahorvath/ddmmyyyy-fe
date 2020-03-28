035b2d98b0ae9ce717ad2d3329677a5f0b8c84c0
helgahorvath <h.horvath302@gmail.com>
2020-03-28 23:36:52 +0100

    posts page, deleted helloworld

---
+++ src/components/Posts.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="posts-container row">
+        <div class="post-container col-4"  v-for="post in posts" :key="post.title">
+            <post :title="post.title" :text="post.text" :tags="post.tags" :create-date="post.createDate" />
+        </div>
+    </div>
+</template>
+
+<script>
+import Post from './Post'
+import postService from '../services/postService'
+export default {
+  name: 'Posts',
+  components: { Post },
+  data () {
+    return {
+      posts: []
+    }
+  },
+  async created () {
+    this.posts = await postService.getPosts()
+  }
+
+}
+</script>
+
+<style scoped lang="scss">
+    .posts-container {
+        border: solid 1px black;
+    }
+
+</style>
